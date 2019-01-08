5e6b3922c0dc247240fdb1a50d332c7d021d0fad
amitJas <katanamit7@gmail.com>
2019-01-08 19:23:03 +0200

    amit

---
+++ HireME app/src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { LoginPage } from '../pages/login/login';
+
+@NgModule({
+	declarations: [LoginPage],
+	imports: [],
+	exports: [LoginPage]
+})
+export class ComponentsModule {}

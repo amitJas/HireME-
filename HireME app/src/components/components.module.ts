141074b92bb67423cc759528a60efa487fb0fe54
amitJas <katanamit7@gmail.com>
2019-01-20 13:54:55 +0200

    cvc

--- HireME app/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LoginPage } from '../pages/login/login';
-
-@NgModule({
-	declarations: [LoginPage],
-	imports: [],
-	exports: [LoginPage]
-})
-export class ComponentsModule {}

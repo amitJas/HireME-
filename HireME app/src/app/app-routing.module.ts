3f4292f47b6ac7ad781d6f3f6192d31645b367d3
amitJas <katanamit7@gmail.com>
2019-03-31 21:07:12 +0300

    fvdg

--- HireME app/src/app/app-routing.module.ts
+++ HireME app/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ const routes: Routes = [
 
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'new-candidate', loadChildren: './new-candidate/new-candidate.module#NewCandidatePageModule' },
   { path: 'candidate', loadChildren: './candidate/candidate.module#CandidatePageModule' },
   { path: 'interview', loadChildren: './interview/interview.module#InterviewPageModule' },
--- HireME app/src/app/firebase-service/firebase-service.service.ts
+++ HireME app/src/app/firebase-service/firebase-service.service.ts
@@ -36,4 +36,7 @@ export class FirebaseService {
     return allCandidteName
   }
 
+  addCandidate(){
+    
+  }
 }

371ccdaef3bfd69fa31d2ffe0b9f6abfa33d6b6a
amitJas <katanamit7@gmail.com>
2019-04-29 12:45:03 +0300

    firebase

--- HireME app/src/app/forms/forms.page.ts
+++ HireME app/src/app/forms/forms.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StationService } from './../station-service/station.service';
+import { initDomAdapter } from '@angular/platform-browser/src/browser';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 
 
@@ -13,17 +15,35 @@ export class FormsPage implements OnInit {
   public sendDate: any;
   public returnDate: any;
   public sendShow = false;
-  
+  private fixdData = true;
   
 
   constructor(private station: StationService ) { }
 
   ngOnInit() {
+    setTimeout( () =>{
+      console.log('this.station', this.station.station)
+      this.initData()
+    },500)
     
- 
+    
+
+    
   }
 
-  
+  initData()
+  {
+    console.log('in initform' ,this.station.station.sendDate )
+    if(this.station.station.sendDate){
+      //this.fixdData = true;
+      this.sendDate = this.station.station.sendDate;
+    }
+    if(this.station.station.returnDate){
+      //this.fixdData = true;
+      this.returnDate = this.station.station.returnDate;
+    }
+    
+  }
   // setData(sendDate){
   //   console.log("in setData",sendDate)
   //   if(this.station.station.sendTime){
@@ -35,12 +55,12 @@ export class FormsPage implements OnInit {
   setDate(date,bool){
     if(bool){
       console.log('send time set',date)
-      //this.station.setFile('sendDate',date)
+      this.station.setFile('sendDate',date)
     }
       
     if(!bool)
       console.log('return time',date)
-      //this.station.setFile('returnDate',date)
+      this.station.setFile('returnDate',date)
   }
   
 }
--- HireME app/src/app/new-candidate/new-candidate.page.ts
+++ HireME app/src/app/new-candidate/new-candidate.page.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { EmailValidator } from '@angular/forms';
-=======
+
 import { FirebaseService } from '../firebase-service/firebase-service.service';
->>>>>>> 3cad537984b88923f13c6737d1c2c9b36e4176af
 
 @Component({
   selector: 'app-new-candidate',
@@ -11,37 +8,9 @@ import { FirebaseService } from '../firebase-service/firebase-service.service';
   styleUrls: ['./new-candidate.page.scss'],
 })
 export class NewCandidatePage implements OnInit {
-public firstname:any;
-public lastname:any;
-public date:Date;
-public id:any;
-public phone:any;
-public email:EmailValidator;
-public password:any;
-public profession: any = {
-  header: 'profession',
-  subHeader: 'מקצוע',
-};
 
-public isExperience:boolean;
-public experienceInYears:Number;
-public madeAnExam:boolean;
-public examGrade:any;
-
-<<<<<<< HEAD
-  
-    public firstName: string
-    public lestName: string
-    public strtProcess: Date;
-    public idd: number
-    public job: string
-    public experience: any
-    public testGraid: any;
-    public phoone: number
-    public emaiil: string
   
     public newC: object;
-=======
     public name: string
     //public lestName: string
     public strtProcess: String;
@@ -52,7 +21,6 @@ public examGrade:any;
     public phone: number
     public email: string
     public lisens: string
->>>>>>> 3cad537984b88923f13c6737d1c2c9b36e4176af
     
     
   constructor(private firebase: FirebaseService) { }

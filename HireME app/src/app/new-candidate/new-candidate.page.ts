e7e5594dad0b8b56670ced887871abef1c4a101e
Karin Marjieh <karinmarjieh@hotmail.com>
2019-04-29 11:39:58 +0300

    thdtfy

--- HireME app/src/app/new-candidate/new-candidate.page.ts
+++ HireME app/src/app/new-candidate/new-candidate.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { EmailValidator } from '@angular/forms';
-=======
 import { FirebaseService } from '../firebase-service/firebase-service.service';
->>>>>>> 3cad537984b88923f13c6737d1c2c9b36e4176af
 
 @Component({
   selector: 'app-new-candidate',
@@ -11,7 +7,7 @@ import { FirebaseService } from '../firebase-service/firebase-service.service';
   styleUrls: ['./new-candidate.page.scss'],
 })
 export class NewCandidatePage implements OnInit {
-public firstname:any;
+/*public firstname:any;
 public lastname:any;
 public date:Date;
 public id:any;
@@ -27,21 +23,7 @@ public isExperience:boolean;
 public experienceInYears:Number;
 public madeAnExam:boolean;
 public examGrade:any;
-
-<<<<<<< HEAD
-  
-    public firstName: string
-    public lestName: string
-    public strtProcess: Date;
-    public idd: number
-    public job: string
-    public experience: any
-    public testGraid: any;
-    public phoone: number
-    public emaiil: string
-  
-    public newC: object;
-=======
+*/
     public name: string
     //public lestName: string
     public strtProcess: String;
@@ -52,7 +34,6 @@ public examGrade:any;
     public phone: number
     public email: string
     public lisens: string
->>>>>>> 3cad537984b88923f13c6737d1c2c9b36e4176af
     
     
   constructor(private firebase: FirebaseService) { }

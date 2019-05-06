69a0ee42134083929e99b1ca4da747f1228a6993
Karin Marjieh <karinmarjieh@hotmail.com>
2019-05-06 12:13:28 +0300

    fgh

--- HireME app/src/app/salary/salary.page.ts
+++ HireME app/src/app/salary/salary.page.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalaryPage implements OnInit {
 
+  public CandidateName : string ; 
+  public allfilesaccepted=false;
+  public date1:Date;
+  public date2:Date;
+  public date3:Date;
+  public date4:Date;
+  public answer:boolean;
+
   constructor() { }
 
   ngOnInit() {

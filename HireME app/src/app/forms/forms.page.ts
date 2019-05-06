8c45c4d4a5abc9b7b87828de5b1dda29823a4d3f
amitJas <katanamit7@gmail.com>
2019-05-06 17:00:10 +0300

    forms

--- HireME app/src/app/forms/forms.page.ts
+++ HireME app/src/app/forms/forms.page.ts
@@ -4,6 +4,7 @@ import { getLocaleDateFormat } from '@angular/common';
 
 
 
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.page.html',
@@ -18,8 +19,14 @@ export class FormsPage implements OnInit {
   public sendShow = false;
   private send = false;
   private return = false;
-  private addForm = false;
-  private f101 = false;
+  private addForm = false ; newForm = String;
+  private f101 = false; 
+  private f102 = false; 
+  private f103 = false; 
+  private f104 = false;
+  private set101 = String; set102 = String; set103 = String; set104 = String;
+  
+
 
   constructor(private station: StationService ) { }
 
@@ -40,15 +47,28 @@ export class FormsPage implements OnInit {
     if(this.station.station.sendDate){ 
       this.sendDate = this.station.station.sendDate.data;
       this.userNameSend = this.station.station.sendDate.how;
-      this.send = true
     }
     if(this.station.station.returnDate){
       this.returnDate = this.station.station.returnDate.data;
       this.userNameReturn = this.station.station.returnDate.how;
-      this.return = true
     }
-    
-   
+    if(this.station.station.f101){ 
+      this.set101 = this.station.station.f101.how;
+      this.f101 = true; 
+      console.log("-----------",this.set101)
+    }
+    if(this.station.station.f102){ 
+      this.set102 = this.station.station.f102.how;
+      this.f102 = true; 
+    }
+    if(this.station.station.f103){ 
+      this.set103 = this.station.station.f103.how;
+      this.f103 = true; 
+    }
+    if(this.station.station.f104){ 
+      this.set104 = this.station.station.f104.how;
+      this.f104 = true; 
+    }
   }
   // setData(sendDate){
   //   console.log("in setData",sendDate)
@@ -63,9 +83,25 @@ export class FormsPage implements OnInit {
   }
 
   addFrom(){
-   
+    // let newDiv = document.createElement('<ion-item> <ion-label>' + this.newForm + '</ion-label></ion-item>'); 
+    // var currentDiv = document.getElementById("div1"); 
+    // document.body.insertBefore(newDiv); 
+    // const element = document.createElement('div');
+    // element.innerHTML = '<ion-item> <ion-label>' + this.newForm + '</ion-label></ion-item>'
+    var temp = document.createElement("temp");
+    var list = document.getElementById("formList");
+      temp.innerHTML = "<ion-item> \
+          <ion-label>" + this.newForm + "</ion-label>\
+          <ion-radio [checked]='f104' slot='start' value='biff'  (ionFocus)='setRdiuoForms('f104')' ></ion-radio>\
+          </ion-item>"
+          list.appendChild(temp);
+    
   }
-  
+  // <ion-item>
+  //           <ion-label  text-right >אישור בטחוני</ion-label>
+  //           <ion-radio [checked]='f104' slot="start" value="biff"  (ionFocus)='setRdiuoForms("f104")' ></ion-radio>
+  //           <ion-note *ngIf='f104' text-right class="userUp" >עודכן ע"י: {{set104}}  </ion-note>
+  //       </ion-item>
   setDate(date,bool){
     if(bool)
       this.station.setFile('sendDate',date) 

20087a2a2aeb6275848c84972eb0ba3e291081d0
amitJas <katanamit7@gmail.com>
2019-05-12 13:19:31 +0300

    de

--- HireME app/src/app/interview/interview.page.ts
+++ HireME app/src/app/interview/interview.page.ts
@@ -10,11 +10,14 @@ import { StationService } from './../station-service/station.service';
 })
 export class InterviewPage implements OnInit {
 
+  public progressCount = 0;
   public candidateName:string;
   public interviewer:string;
-  public date:Date;
-  public discription:any;
-  public pass:boolean;
+  public date:any;
+  //public discription:any;
+  //public pass:boolean;
+  public interviewerName = String
+  public userSetInterviewer = String
   
   constructor(private station: StationService ) {
 
@@ -23,6 +26,26 @@ export class InterviewPage implements OnInit {
   ngOnInit() {
     this.candidateName = this.station.candidateName
     console.log(this.candidateName);
+  }
+
+  initInterview(){
+    if(this.station.station.interviewer){
+      this.interviewerName = this.station.station.interviewer
+      this.userSetInterviewer = this.station.station.interviewer.how;
+      this.progressCount++
+    }
+    if(this.station.station.interviewDate){
+      this.interviewerName = this.station.station.interviewDate
+      this.userSetInterviewer = this.station.station.interviewDate.how;
+      this.progressCount++
+    } 
+  }
+
+  setData(data,num){
+    if(num == 1)
+      this.station.setFile('interviewer',data) 
+    if(num == 2)
+      this.station.setFile('interviewDate',data)
   }
 
 }
